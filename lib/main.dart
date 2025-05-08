5e0906526bb93dc06e565291c24f2effb5db370c
Poggrz <99813849+Poggrz@users.noreply.github.com>
2025-05-08 01:11:10 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,221 +1,86 @@
-import 'package:math_expressions/math_expressions.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import './widgets/CalcButton.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(CalcApp());
-}
-
-class CalcApp extends StatefulWidget {
-  const CalcApp({Key key}) : super(key: key);
-
+class MyApp extends StatefulWidget {
   @override
-  CalcAppState createState() => CalcAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class CalcAppState extends State<CalcApp> {
-  String _history = '';
-  String _expression = '';
+class _MyAppState extends State<MyApp> {
+  String _input = '';
+  String _result = '';
 
-  void numClick(String text) {
-    setState(() => _expression += text);
-  }
-
-  void allClear(String text) {
+  void _buttonPressed(String text) {
     setState(() {
-      _history = '';
-      _expression = '';
+      if (text == 'C') {
+        _input = '';
+        _result = '';
+      } else if (text == '=') {
+        _calculate();
+      } else {
+        _input += text;
+      }
     });
   }
 
-  void clear(String text) {
-    setState(() {
-      _expression = '';
-    });
+  void _calculate() {
+    try {
+      // Basic support for +, -, *, /
+      if (_input.contains('+')) {
+        var parts = _input.split('+');
+        _result = (double.parse(parts[0]) + double.parse(parts[1])).toString();
+      } else if (_input.contains('-')) {
+        var parts = _input.split('-');
+        _result = (double.parse(parts[0]) - double.parse(parts[1])).toString();
+      } else if (_input.contains('*')) {
+        var parts = _input.split('*');
+        _result = (double.parse(parts[0]) * double.parse(parts[1])).toString();
+      } else if (_input.contains('/')) {
+        var parts = _input.split('/');
+        _result = (double.parse(parts[0]) / double.parse(parts[1])).toString();
+      } else {
+        _result = 'Invalid';
+      }
+    } catch (e) {
+      _result = 'Error';
+    }
   }
 
-  void evaluate(String text) {
-    Parser p = Parser();
-    Expression exp = p.parse(_expression);
-    ContextModel cm = ContextModel();
-
-    setState(() {
-      _history = _expression;
-      _expression = exp.evaluate(EvaluationType.REAL, cm).toString();
-    });
+  Widget buildButton(String text) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => _buttonPressed(text),
+        child: Text(text, style: TextStyle(fontSize: 24)),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator',
       home: Scaffold(
-        backgroundColor: Color(0xFF283637),
-        body: Container(
-          padding: EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 12),
-                  child: Text(
-                    _history,
-                    style: GoogleFonts.rubik(
-                      textStyle: TextStyle(
-                        fontSize: 24,
-                        color: Color(0xFF545F61),
-                      ),
-                    ),
-                  ),
+        appBar: AppBar(title: Text('Simple Calc')),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(24),
+                alignment: Alignment.bottomRight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_input, style: TextStyle(fontSize: 32)),
+                    Text(_result, style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                  ],
                 ),
-                alignment: Alignment(1.0, 1.0),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Text(
-                    _expression,
-                    style: GoogleFonts.rubik(
-                      textStyle: TextStyle(
-                        fontSize: 48,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                alignment: Alignment(1.0, 1.0),
-              ),
-              SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CalcButton(
-                    text: 'AC',
-                    fillColor: 0xFF6C807F,
-                    textSize: 20,
-                    callback: allClear,
-                  ),
-                  CalcButton(
-                    text: 'C',
-                    fillColor: 0xFF6C807F,
-                    callback: clear,
-                  ),
-                  CalcButton(
-                    text: '%',
-                    fillColor: 0xFFFFFFFF,
-                    textColor: 0xFF65BDAC,
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '/',
-                    fillColor: 0xFFFFFFFF,
-                    textColor: 0xFF65BDAC,
-                    callback: numClick,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CalcButton(
-                    text: '7',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '8',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '9',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '*',
-                    fillColor: 0xFFFFFFFF,
-                    textColor: 0xFF65BDAC,
-                    textSize: 24,
-                    callback: numClick,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CalcButton(
-                    text: '4',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '5',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '6',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '-',
-                    fillColor: 0xFFFFFFFF,
-                    textColor: 0xFF65BDAC,
-                    textSize: 38,
-                    callback: numClick,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CalcButton(
-                    text: '1',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '2',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '3',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '+',
-                    fillColor: 0xFFFFFFFF,
-                    textColor: 0xFF65BDAC,
-                    textSize: 30,
-                    callback: numClick,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CalcButton(
-                    text: '.',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '0',
-                    callback: numClick,
-                  ),
-                  CalcButton(
-                    text: '00',
-                    callback: numClick,
-                    textSize: 26,
-                  ),
-                  CalcButton(
-                    text: '=',
-                    fillColor: 0xFFFFFFFF,
-                    textColor: 0xFF65BDAC,
-                    callback: evaluate,
-                  ),
-                ],
-              )
-            ],
-          ),
+            ),
+            Row(children: [buildButton('7'), buildButton('8'), buildButton('9'), buildButton('/')]),
+            Row(children: [buildButton('4'), buildButton('5'), buildButton('6'), buildButton('*')]),
+            Row(children: [buildButton('1'), buildButton('2'), buildButton('3'), buildButton('-')]),
+            Row(children: [buildButton('0'), buildButton('C'), buildButton('='), buildButton('+')]),
+          ],
         ),
       ),
     );
